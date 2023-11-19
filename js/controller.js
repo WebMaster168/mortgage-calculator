753914180d0ac8da451447a75f3e3dd680941a18
Ilya <shcherbakov.illya@list.ru>
2023-11-19 14:50:59 +0400

    initial percent

---
+++ js/controller.js
@@ -0,0 +1,15 @@
+import * as Model from './model.js'
+import programs from './view/radioPrograms.js'
+
+window.onload = function () {
+    const getData = Model.getData
+
+    // Init programs
+    programs(getData)
+
+    document.addEventListener('updateForm', (e) => {
+        Model.setData(e.detail)    
+    })
+
+
+}
